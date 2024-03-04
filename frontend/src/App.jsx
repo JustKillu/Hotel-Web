60120c94dff807e474b865730aef9ebded615591
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-04 16:53:08 -0400

    Se añadieron nuevas rutas para el cliente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,26 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import Register from './pages/Register.jsx'; 
 import Login from './pages/Login.jsx';
-import Navbar from './components/NavBar.jsx';
 import Admin from './pages/Admin.jsx';
+import Reserva from './pages/Reserva.jsx';
+import Perfil from './pages/Perfil.jsx';
+import BlogAndGuidePage from './pages/BlogAndGuidePage.jsx';
+
+
 
 
 function App() {
   return (
     <Router>
-      <Navbar /> 
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} /> 
         <Route path="/admin" element={<Admin />} /> 
+        <Route path="/reserva" element={<Reserva />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/guide" element={<BlogAndGuidePage />} />
       </Routes>
     </Router>
   );
