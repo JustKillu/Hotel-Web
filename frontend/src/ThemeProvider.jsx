adce490e879df2eabb6b0ccae7a79932e4a0abd0
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-18 18:39:33 -0400

    Archivo para manejar la logica del darkmode

---
+++ frontend/src/ThemeProvider.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const ThemeContext = React.createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  React.useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
