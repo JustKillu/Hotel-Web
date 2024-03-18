61ecf4668afd279b21089745acd1a653e9fc3bf2
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-18 18:39:18 -0400

    Pagina para visualizar las rooms

--- frontend/src/pages/Rooms.jsx
+++ frontend/src/pages/Rooms.jsx
@@ -14,6 +14,7 @@ const HotelRooms = () => {
     const [roomReviews, setRoomReviews] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const reviewsPerPage = 3; // Número de reseñas por página
+    const [roomSpaceFilter, setRoomSpaceFilter] = useState(''); 
 
     useEffect(() => {
         axios.get('http://localhost:3001/rooms')
@@ -36,11 +37,14 @@ const HotelRooms = () => {
 
     const handleReviewSubmit = (roomId) => {
         const user = localStorage.getItem('user');
-
-        axios.post(`http://localhost:3001/rooms/${roomId}/reviews`, { text: reviews[roomId], user: user })
+        const date = new Date();
+        const review = { text: reviews[roomId], user: user, date: date };
+    
+        axios.post(`http://localhost:3001/rooms/${roomId}/reviews`, review)
             .then(response => {
                 console.log('Review enviada: ', response.data);
                 setError(null);
+                setRoomReviews(prevReviews => [...prevReviews, review]);
             })
             .catch(error => {
                 console.error('Error al enviar review: ', error);
@@ -48,7 +52,9 @@ const HotelRooms = () => {
                 setError('Error enviando la review');
             });
     };
-
+    
+    
+    
     const handleRoomSelect = (room) => {
         setSelectedRoom(room);
         axios.get(`http://localhost:3001/rooms/${room._id}/reviews`)
@@ -69,11 +75,12 @@ const HotelRooms = () => {
     if (loading) return "Cargando...";
 
     return (
-        <div className="bg-white min-h-screen text-black pt-12">
+        <div className="min-h-screen text-black pt-12">
             <NavBarMain/>
             <div className="container mx-auto px-4 ">
+                <input type="text" value={roomSpaceFilter} onChange={(e) => setRoomSpaceFilter(e.target.value)} placeholder="Filtrar por espacio" className="w-full p-2 border mt-12 rounded mb-2" />
                 <div className="grid grid-cols-3 gap-4">
-                    {rooms.map(room => {
+                {rooms.filter(room => room && room.espacio && room.espacio.includes(roomSpaceFilter)).map(room => {
                         let imgSrc = '';
                         if (room.img && room.img.data && room.img.data.data) {
                             imgSrc = `data:${room.img.contentType};base64,${Buffer.from(room.img.data.data).toString('base64')}`;
@@ -93,41 +100,40 @@ const HotelRooms = () => {
                 </div>
             </div>
             {selectedRoom && (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10">
-        <div className="bg-white p-6 rounded flex">
-            <img src={`data:${selectedRoom.img.contentType};base64,${Buffer.from(selectedRoom.img.data.data).toString('base64')}`} alt="" className="w-64 h-64 object-cover mb-4" loading="lazy" />
-            <div className="ml-4">
-                <h2 className="text-2xl font-semibold mb-2">{selectedRoom.name}</h2>
-                <p className="mb-2"><strong>Descripción:</strong> {selectedRoom.description}</p>
-                <p className="mb-2"><strong>Comodidades:</strong> {selectedRoom.comodidades}</p>
-                <p className="mb-2"><strong>Tarifas:</strong> {selectedRoom.tarifas}</p>
-                <p className="mb-2"><strong>Evaluación:</strong> {selectedRoom.evaluacion}</p>
-                <p className="mb-2"><strong>Reseñas:</strong></p>
-                <div className="border p-2 mb-2 rounded">
-                    {currentReviews.length > 0 ? currentReviews.map((review, index) => (
-                        <div key={index} className="border-b last:border-b-0 p-2">
-                            <p><strong>Usuario:</strong> {review.user}</p>
-                            <p><strong>Reseña:</strong> {review.text}</p>
-                            <p className="text-sm text-gray-500">Fecha: {new Date(review.date).toLocaleDateString()}</p>
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10">
+                    <div className="bg-white p-6 rounded flex">
+                        <img src={`data:${selectedRoom.img.contentType};base64,${Buffer.from(selectedRoom.img.data.data).toString('base64')}`} alt="" className="w-64 h-64 object-cover mb-4" loading="lazy" />
+                        <div className="ml-4">
+                            <h2 className="text-2xl font-semibold mb-2">{selectedRoom.name}</h2>
+                            <p className="mb-2"><strong>Descripción:</strong> {selectedRoom.description}</p>
+                            <p className="mb-2"><strong>Comodidades:</strong> {selectedRoom.comodidades}</p>
+                            <p className="mb-2"><strong>Tarifas:</strong> {selectedRoom.tarifas}</p>
+                            <p className="mb-2"><strong>Evaluación:</strong> {selectedRoom.evaluacion}</p>
+                            <p className="mb-2"><strong>Espacio:</strong>{selectedRoom.espacio}</p>
+                            <p className="mb-2"><strong>Reseñas:</strong></p>
+                            <div className="border p-2 mb-2 rounded">
+                                {currentReviews.length > 0 ? currentReviews.map((review, index) => (
+                                    <div key={index} className="border-b last:border-b-0 p-2">
+                                        <p><strong>Usuario:</strong> {review.user}</p>
+                                        <p><strong>Reseña:</strong> {review.text}</p>
+                                        <p className="text-sm text-gray-500">Fecha: {new Date(review.date).toLocaleDateString()}</p>
+                                    </div>
+                                )) : <p>No hay reseñas</p>}
+                            </div>
+                            <Pagination>
+                                {[...Array(Math.ceil(roomReviews.length / reviewsPerPage)).keys()].map(number => (
+                                    <Pagination.Item key={number+1} active={number+1 === currentPage} onClick={() => paginate(number+1)}>
+                                        {number+1}
+                                    </Pagination.Item>
+                                ))}
+                            </Pagination>
+                            <textarea value={reviews[selectedRoom._id] || ''} onChange={(event) => handleReviewChange(selectedRoom._id, event)} className="w-full p-2 border rounded mb-2" placeholder="Deja una reseña" />
+                            <button onClick={() => handleReviewSubmit(selectedRoom._id)} className="w-full py-2 bg-blue-500 text-light-text dark:text-dark-text  rounded">Enviar reseña</button>
+                            <button onClick={() => setSelectedRoom(null)} className="w-full py-2 mt-4 text-center text-light-text dark:text-dark-text  bg-red-500 rounded">Cerrar</button>
                         </div>
-                    )) : <p>No hay reseñas</p>}
+                    </div>
                 </div>
-                <Pagination>
-                    {[...Array(Math.ceil(roomReviews.length / reviewsPerPage)).keys()].map(number => (
-                        <Pagination.Item key={number+1} active={number+1 === currentPage} onClick={() => paginate(number+1)}>
-                            {number+1}
-                        </Pagination.Item>
-                    ))}
-                </Pagination>
-                <textarea value={reviews[selectedRoom._id] || ''} onChange={(event) => handleReviewChange(selectedRoom._id, event)} className="w-full p-2 border rounded mb-2" placeholder="Deja una reseña" />
-                <button onClick={() => handleReviewSubmit(selectedRoom._id)} className="w-full py-2 bg-blue-500 text-white rounded">Enviar reseña</button>
-                <button onClick={() => setSelectedRoom(null)} className="w-full py-2 mt-4 text-center text-white bg-red-500 rounded">Cerrar</button>
-            </div>
-        </div>
-    </div>
-)}
-
-
+            )}
         </div>
     );
 }
