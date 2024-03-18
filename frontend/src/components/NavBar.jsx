9c332bd6b6c11c146b4768d7de47e3f2d356fea8
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-18 18:36:03 -0400

    Cambios en la nav  y nuevos estados para el menu

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
+import DarkMode from '../components/DarkMode.jsx';
+
 const Navbar = () => {
   const [user, setUser] = useState(localStorage.getItem('user'));
   const [role, setRole] = useState(localStorage.getItem('rol'));
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-
+  const [isMenuOpenInicio, setIsMenuOpenInicio] = useState(false); // Nuevo estado para el menú de Inicio
+  const [isMenuOpenLogin, setIsMenuOpenLogin] = useState(false); // Nuevo estado para el menú de Iniciar sesión
+  const menuRef = useRef(null);
   let timeoutId;
 
   useEffect(() => {
@@ -27,18 +30,38 @@ const Navbar = () => {
     setRole(null);
   };
 
-  const handleMouseOver = () => {
+  const handleMouseOverInicio = () => {
     clearTimeout(timeoutId);
-    setIsLoginOpen(true);
+    timeoutId = setTimeout(() => {
+      setIsMenuOpenInicio(true);
+    }, 500);
   };
 
-  const handleMouseOut = () => {
+  const handleMouseOutInicio = () => {
+    clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
-      setIsLoginOpen(false);
-    }, 200);
+      setIsMenuOpenInicio(false);
+    }, 500); 
   };
+
+  const handleMouseOverLogin = () => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      setIsMenuOpenLogin(true);
+    }, 200); 
+  };
+
+  const handleMouseOutLogin = () => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      setIsMenuOpenLogin(false);
+    }, 500);
+  };
+
+
   return (
     <nav className="fixed top-0 w-full z-50 flex items-center justify-between p-6 bg-brown bg-opacity-80 text-white">
+      <DarkMode/>
       <div className="flex items-center flex-shrink-0 mr-6">
         <span className="font-semibold text-xl tracking-tight">Hotel Valera</span>
       </div>
@@ -49,63 +72,79 @@ const Navbar = () => {
       </div>
       <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isNavOpen ? '' : 'hidden'}`}>
         <div className="text-sm lg:flex-grow">
-          <ScrollLink to="#" smooth duration={500} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-            Inicio
-          </ScrollLink>
-          <ScrollLink to="nosotros" smooth duration={500} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-            Nosotros
-          </ScrollLink>
-          <ScrollLink to="habitaciones" smooth duration={500} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-            Habitaciones
-          </ScrollLink>
-          <ScrollLink to="banquetes" smooth duration={500} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-            Banquetes
-          </ScrollLink>
-          <ScrollLink to="areas" smooth duration={500} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-            Areas
-          </ScrollLink>
-          <Link to="/reserva" smooth duration={500} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-            Reserva
-          </Link>
-          {user ? (
-            <>
-              <Link to="/perfil" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-                Perfil
-              </Link>
-              <Link to="/guide" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-                Guia
-              </Link>
-              <button onClick={handleLogout} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:underline">
-                Cerrar sesión
+          <div className="relative inline-block text-left" onMouseOver={handleMouseOverInicio} onMouseOut={handleMouseOutInicio}>
+            <div>
+              <button type="button" className="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 text-sm font-medium hover:underline">
+                Inicio
               </button>
-            </>
-          ) : (
-            <div className="relative inline-block text-left" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-              <div>
-                <button type="button" className="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 text-sm font-medium hover:underline">
-                  Iniciar sesión
-                </button>
-              </div>
+            </div>
 
-              {isLoginOpen && (
-                <div className="origin-top-right  absolute right-0 mt-2 w-56 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
-                  <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                    <Link to="/login" className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">Iniciar sesión</Link>
-                    <Link to="/register" className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">Registrarse</Link>
-                  </div>
-                </div>
-              )}
+            {isMenuOpenInicio && (
+              <div className="origin-top-right  absolute right-0 mt-2 w-56 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <ScrollLink to="#" smooth duration={500} className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">
+                    Inicio
+                  </ScrollLink>
+                  <ScrollLink to="nosotros" smooth duration={500} className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">
+                    Nosotros
+                  </ScrollLink>
+                  <ScrollLink to="habitaciones" smooth duration={500} className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">
+                    Habitaciones
+                  </ScrollLink>
+                  <ScrollLink to="banquetes" smooth duration={500} className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">
+                    Banquetes
+                  </ScrollLink>
+                  <ScrollLink to="areas" smooth duration={500} className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">
+                    Areas
+                  </ScrollLink>
+                  <Link to="/reserva" smooth duration={500} className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">
+                    Reserva
+                  </Link>
+              </div>
             </div>
           )}
-          {role === 'adm' && (
-            <Link to="/admin" className="block mt-4 lg:inline-block lg:mt-0 hover:underline">
-              Admin
-            </Link>
-          )}
         </div>
+        {user ? (
+          <>
+            <Link to="/perfil" className="block mt-4 lg:inline-block lg:mt-0 mr-2 hover:underline">
+              Perfil
+            </Link>
+            <Link to="/guide" className="block mt-4 lg:inline-block lg:mt-0 mr-2 hover:underline">
+              Guia
+            </Link>
+            <Link to="/rooms" className="block mt-4 lg:inline-block lg:mt-0 mr-2 hover:underline">
+              Habitaciones
+            </Link>
+            <button onClick={handleLogout} className="block mt-4 lg:inline-block lg:mt-0 mr-2 hover:underline">
+              Cerrar sesión
+            </button>
+          </>
+        ) : (
+          <div className="relative inline-block text-left" onMouseOver={handleMouseOverLogin} onMouseOut={handleMouseOutLogin}>
+            <div>
+              <button type="button" className="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 text-sm font-medium hover:underline">
+                Iniciar sesión
+              </button>
+            </div>
+
+            {isMenuOpenLogin && (
+              <div className="origin-top-right  absolute right-0 mt-2 w-56 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <Link to="/login" className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">Iniciar sesión</Link>
+                  <Link to="/register" className="block px-4 py-2 text-sm text-white bg-brown hover:bg-pastel" role="menuitem">Registrarse</Link>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+        {role === 'adm' && (
+          <Link to="/admin" className="block mt-4 lg:inline-block lg:mt-0 hover:underline">
+            Admin
+          </Link>
+        )}
       </div>
-    </nav>
-  );
-};
+    </div>
+  </nav>
+);}
 
 export default Navbar;
