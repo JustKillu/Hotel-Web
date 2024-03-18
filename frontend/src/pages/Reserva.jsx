d8ed2bf49bb90a4405e794b151932d6059d28883
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-18 18:38:44 -0400

    Darkmode y se actualizo para visualizar las imagenes guardadas

--- frontend/src/pages/Reserva.jsx
+++ frontend/src/pages/Reserva.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavBarMain from '../components/NavBarMain.jsx';
+import { Buffer } from 'buffer';
 
 const Reserva = () => {
   const [rooms, setRooms] = useState([]);
@@ -100,7 +101,7 @@ const Reserva = () => {
       <NavBarMain />
       <div className='absolute top-0 right-0 m-4'>
         {alert && (
-          <div className={`alert ${alert.type === 'error' ? 'bg-red-500' : 'bg-green-500'} text-white px-6 py-4 rounded mb-4 w-full max-w-md`}>
+          <div className={`alert ${alert.type === 'error' ? 'bg-red-500' : 'bg-green-500'} text-light-text dark:text-dark-text  px-6 py-4 rounded mb-4 w-full max-w-md`}>
             {alert.message}
           </div>
         )}
@@ -109,47 +110,66 @@ const Reserva = () => {
         <form className="flex flex-col w-full max-w-md bg-white text-black rounded-xl p-4" onSubmit={handleSubmit}>
           <div className="mb-2">
             Habitaciones:
-            {Array.isArray(rooms) && rooms.map(room => (
-              <label key={room.id} className="block relative">
-                <input type="checkbox" name={room.name} onChange={handleCheckboxChange} onMouseEnter={() => setHoveredRoom(room)} onMouseLeave={() => setHoveredRoom(null)} />
-                <span className="ml-2 ">{room.name}</span>
-                {hoveredRoom === room && (
-                  <div className=" p-4 border rounded-xl border-gray-700 bg-white absolute ml-64  w-full z-10">
-                    <img src={room.img} alt={room.name} className="w-full h-32 object-cover mb-2 " />
-                    <h2 className="font-bold mb-2 bg-white">{room.name}</h2>
-                    <p className='text-green-500'>{room.price}<span className=' text-pink-600' > Duración {room.time}</span></p>
-                    <p>{room.description}</p>
-                  </div>
-                )}
-              </label>
-            ))}
+            {Array.isArray(rooms) && rooms.map(room => {
+  let imgSrc = '';
+  if (room.img && room.img.data && room.img.data.data) {
+    imgSrc = `data:${room.img.contentType};base64,${Buffer.from(room.img.data.data).toString('base64')}`;
+  }
+  return (
+    <label key={room.id} className="block relative" onMouseEnter={() => setHoveredRoom(room)} onMouseLeave={() => setHoveredRoom(null)}>
+      <input type="checkbox" name={room.name} onChange={handleCheckboxChange} onMouseEnter={() => setHoveredRoom(room)} onMouseLeave={() => setHoveredRoom(null)} />
+      <span className="ml-2 ">{room.name}</span>
+      {hoveredRoom === room && (
+        <div className=" p-4 border rounded-xl border-gray-700 bg-white absolute ml-64  w-full z-10">
+          <img src={imgSrc} alt={room.name} className="w-full h-32 object-cover mb-2 " />
+          <h2 className="text-2xl font-semibold mb-2">{room.name}</h2>
+          <p className="mb-2"><strong>Descripción:</strong> {room.description}</p>
+          <p className="mb-2"><strong>Comodidades:</strong> {room.comodidades}</p>
+          <p className="mb-2"><strong>Tarifas:</strong> {room.tarifas}</p>
+          <p className="mb-2"><strong>Evaluación:</strong> {room.evaluacion}</p>
+          <p className="mb-2"><strong>Reseñas:</strong></p>
+        </div>
+      )}
+    </label>
+  );
+})}
+
           </div>
           <div className="mb-2">
-            Promociones:
-            {Array.isArray(promotions) && promotions.map(promotion => (
-              <label key={promotion.id} className="block relative bg-yellow-200 p-2 rounded-md">
-                <input type="checkbox" name={promotion.name} onChange={handlePromotionChange} onMouseEnter={() => setHoveredPromotion(promotion)} onMouseLeave={() => setHoveredPromotion(null)} />
-                <span className="ml-2">{promotion.name}</span>
-                {hoveredPromotion === promotion && (
-                  <div className=" p-4 border rounded-xl border-gray-700 bg-white absolute ml-64  w-full z-10">
-                    <img src={promotion.image} alt={promotion.name} className="w-full h-32 object-cover mb-2 " />
-                    <h2 className="font-bold mb-2 bg-white">{promotion.name}</h2>
-                    <p className='text-green-500'>{promotion.price}$<span className='text-pink-600'>  Duracion de promoción:{promotion.time}</span></p>
-                    <p>{promotion.description}</p>
-                  </div>
-                )}
-              </label>
-            ))}
-          </div>
-          <label className="mb-2 ">
-            Fecha:
-            <input className="border-2 bg-gray-200  border-gray-300 p-2" type="date" name="date" onChange={handleDateChange} />
-          </label>
-          <button className="bg-blue-500 text-white p-2 mt-4" type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
+  Promociones:
+  {Array.isArray(promotions) && promotions.map(promotion => {
+  let imgSrc = '';
+  if (promotion.img && promotion.img.data && promotion.img.data.data) {
+    imgSrc = `data:${promotion.img.contentType};base64,${Buffer.from(promotion.img.data.data).toString('base64')}`;
+  }
+  return (
+    <label key={promotion.id} className="block relative" onMouseEnter={() => setHoveredPromotion(promotion)} onMouseLeave={() => setHoveredPromotion(null)}>
+      <input type="checkbox" name={promotion.name} onChange={handlePromotionChange} onMouseEnter={() => setHoveredPromotion(promotion)} onMouseLeave={() => setHoveredPromotion(null)} />
+      <span className="ml-2">{promotion.name}</span>
+      {hoveredPromotion === promotion && (
+        <div className=" p-4 border rounded-xl border-gray-700 bg-white absolute ml-64  w-full z-10">
+          <img src={imgSrc} alt={promotion.name} className="w-full h-32 object-cover mb-2 " />
+          <h2 className="text-2xl font-semibold mb-2">{promotion.name}</h2>
+          <p className="mb-2"><strong>Descripción:</strong> {promotion.description}</p>
+          <p className="mb-2"><strong>Comodidades:</strong> {promotion.comodidades}</p>
+          <p className="mb-2"><strong>Tarifas:</strong> {promotion.tarifas}</p>
+          <p className="mb-2"><strong>Evaluación:</strong> {promotion.evaluacion}</p>
+          <p className="mb-2"><strong>Reseñas:</strong></p>
+        </div>
+      )}
+    </label>
   );
-};
+})}
+
+</div>
+<label className="mb-2 ">
+  Fecha:
+  <input className="border-2 bg-gray-200  border-gray-300 p-2" type="date" name="date" onChange={handleDateChange} />
+</label>
+<button className="bg-blue-500 text-light-text dark:text-dark-text  p-2 mt-4" type="submit">Enviar</button>
+</form>
+</div>
+</div>
+);}
 
 export default Reserva;
