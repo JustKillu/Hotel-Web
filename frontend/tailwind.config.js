0339cb9079f0e1ac0133641fed4ab29af04b6088
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-18 18:39:47 -0400

    Se añadio los valores que van en darkmode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,15 +3,20 @@ export default {
   content: [
     './src/**/*.jsx',
     './src/**/*.css',
-    
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      
       colors: {
         pastel: '#f3dabc',
-        brown:'#6D4C3C'
-      }
+        brown:'#6D4C3C',
+        'light-text': '#000000', 
+        'dark-text': '#ffffff',
+      },
+      backgroundImage: theme => ({
+        'light-gradient': 'linear-gradient(to right, #ffffff, #f3dabc)',
+        'dark-gradient': 'linear-gradient(to right, #242424, #6D4C3C)',
+      })
     },
   },
   plugins: [],
