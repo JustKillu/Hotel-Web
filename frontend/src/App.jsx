81dbd6e1bac075eb2d6a384985e2c129050e663a
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-18 18:33:49 -0400

    Se añadio el themeprovider para manejar darkmode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,24 +8,24 @@ import Reserva from './pages/Reserva.jsx';
 import Perfil from './pages/Perfil.jsx';
 import BlogAndGuidePage from './pages/BlogAndGuidePage.jsx';
 import Rooms from './pages/Rooms.jsx';
-
-
+import { ThemeProvider, ThemeContext } from './ThemeProvider'; 
 
 function App() {
   return (
-    <Router>
-      
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/admin" element={<Admin />} /> 
-        <Route path="/reserva" element={<Reserva />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/guide" element={<BlogAndGuidePage />} />
-        <Route path="/rooms" element={<Rooms />} />
-      </Routes>
-    </Router>
+    <ThemeProvider> 
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/admin" element={<Admin />} /> 
+          <Route path="/reserva" element={<Reserva />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/guide" element={<BlogAndGuidePage />} />
+          <Route path="/rooms" element={<Rooms />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
