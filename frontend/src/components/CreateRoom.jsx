ba08628378ee39bd77d4d3f10f279096735e5cab
Killu <60795569+JustKillu@users.noreply.github.com>
2024-03-18 18:34:52 -0400

    Componente nuevo para crear habitaciones

--- frontend/src/components/CreateRoom.jsx
+++ frontend/src/components/CreateRoom.jsx
@@ -9,8 +9,10 @@ const CreateRoom = () => {
         tarifas: '',
         reviews: '',
         evaluacion: '',
-        img: null
+        img: null,
+        espacio: ''
     });
+    const [showCard, setShowCard] = useState(false);
 
     const handleInputChange = (event) => {
         setRoom({
@@ -42,33 +44,51 @@ const CreateRoom = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type="text" name="name" onChange={handleInputChange} />
-            </label>
-            <label>
-                Description:
-                <input type="text" name="description" onChange={handleInputChange} />
-            </label>
-            <label>
-                Comodidades:
-                <input type="text" name="comodidades" onChange={handleInputChange} />
-            </label>
-            <label>
-                Tarifas:
-                <input type="text" name="tarifas" onChange={handleInputChange} />
-            </label>
-            <label>
-                Evaluacion:
-                <input type="text" name="evaluacion" onChange={handleInputChange} />
-            </label>
-            <label>
-                Image:
-                <input type="file" name="img" onChange={handleFileChange} />
-            </label>
-            <button type="submit">Create Room</button>
-        </form>
+        <div className="flex flex-col items-start justify-between ">
+            {showCard && (
+                <form onSubmit={handleSubmit} className="mt-4 mb-56 ml-14 p-4 border rounded shadow-lg bg-white fixed bottom-0 left-0 z-50">
+                    <label className="block">
+                        <span className="text-gray-700">Name:</span>
+                        <input type="text" name="name" onChange={handleInputChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                    </label>
+                    <label className="block mt-4">
+                        <span className="text-gray-700">Description:</span>
+                        <input type="text" name="description" onChange={handleInputChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                    </label>
+                    <label className="block mt-4">
+                        <span className="text-gray-700">Comodidades:</span>
+                        <input type="text" name="comodidades" onChange={handleInputChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                    </label>
+                    <label className="block mt-4">
+                        <span className="text-gray-700">Tarifas:</span>
+                        <input type="text" name="tarifas" onChange={handleInputChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                    </label>
+                    <label className="block mt-4">
+                        <span className="text-gray-700">Evaluacion:</span>
+                        <input type="text" name="evaluacion" onChange={handleInputChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                    </label>
+                    <label className="block mt-4">
+                        <span className="text-gray-700">Espacio:</span>
+                        <input type="text" name="espacio" onChange={handleInputChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                    </label>
+                    <label className="block mt-4">
+                        <span className="text-gray-700">Image:</span>
+                        <input type="file" name="img" onChange={handleFileChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                    </label>
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+                        Enviar
+                    </button>
+                </form>
+ )}
+
+<button 
+    className="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-full fixed bottom-8 left-0 z-50"
+    onClick={() => setShowCard(!showCard)}
+>
+    Crear Habitación
+</button>
+
+</div>
     );
 };
 
